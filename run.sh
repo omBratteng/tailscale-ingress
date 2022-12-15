cc7037666af815dfb236314d7edb3d273269847b
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2022-12-15 17:48:40 +0100

    Fix run.sh in 1.34

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env sh
 # Copyright (c) 2022 Tailscale Inc & AUTHORS All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-#! /bin/sh
 
+set -xe
 export PATH=$PATH:/tailscale/bin
 
 TS_AUTH_KEY="${TS_AUTH_KEY:-}"

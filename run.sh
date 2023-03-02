b7c1bf93c4ea7cd7fe039c0a89d69813b6625796
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-03-02 13:19:36 +0100

    Add port 3000 redirect

--- run.sh
+++ run.sh
@@ -61,6 +61,7 @@ echo "allow *.*.*.*" > /etc/rinetd.conf
 echo "logfile /dev/stdout" >> /etc/rinetd.conf
 echo "0.0.0.0  80   ${REDIRECT_TARGET}   80" >> /etc/rinetd.conf
 echo "0.0.0.0  443  ${REDIRECT_TARGET}   443" >> /etc/rinetd.conf
+echo "0.0.0.0  3000  100.71.44.50   3000" >> /etc/rinetd.conf
 
 rinetd -f -c /etc/rinetd.conf &
 wait ${PID}
